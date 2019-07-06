f00983c00eb4d912c34ced21e1a1987c5918f30c
Reece Pritchard <rapritchard96@gmail.com>
2019-07-06 17:13:09 +0100

    Organised main project for react+redux

---
+++ src/reducers/expenses.js
@@ -0,0 +1,26 @@
+// Expenses Reducer
+const exspensesReducerDefaultState = [];
+
+export default (state = exspensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense,
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        }
+        return expense;
+      });
+    default:
+      return state;
+  }
+};
