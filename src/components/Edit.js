4deb016cc38b482b6a2d10218d6c76740e2b408b
Reece Pritchard <rapritchard96@gmail.com>
2019-07-07 19:38:53 +0100

    Made ExpenseForm reusable for add / edit expense

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
-const Edit = (_props) => {
-  console.log(_props);
-  return (
-    <div>
-      Editing the expense with ID of
-      {_props.match.params.id}
-    </div>
-  );
-};
 
-export default Edit;
+const Edit = props => (
+  <div>
+    <ExpenseForm
+      expense={props.expense}
+      onSubmit={(expense) => {
+        props.dispatch(editExpense(props.expense.id, expense));
+        props.history.push('/');
+      }}
+    />
+    <button onClick={() => {
+      props.dispatch(removeExpense({ id: props.expense.id }));
+      props.history.push('/');
+    }}
+    >
+    Remove
+    </button>
+  </div>
+);
+
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find(expense => expense.id === props.match.params.id),
+});
+
+export default connect(mapStateToProps)(Edit);
