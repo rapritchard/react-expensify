d6407695c78229491ede24ee37ee5908eed7f2b1
Reece Pritchard <rapritchard96@gmail.com>
2019-07-07 19:06:10 +0100

    Wired up add expense so that expenses are added and redirected to dashboard.

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const Create = () => (
+const Create = (props) => (
   <div>
     <h1>Add Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm 
+      onSubmit={(expense) => {
+        props.dispatch(addExpense(expense));
+        props.history.push('/');
+      }}
+    />
   </div>
 );
 
-export default Create;
+export default connect()(Create);
