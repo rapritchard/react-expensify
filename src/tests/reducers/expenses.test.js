1e9f1a81357fa662506f228d0d650705fefd6bc0
Reece Pritchard <rapritchard96@gmail.com>
2019-07-12 21:16:02 +0100

    Added test suite for expenses reducer

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,60 @@
+import expenseReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+
+test('should setup default expenses state ', () => {
+  const state = expenseReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('should remove an expense', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id,
+  };
+  const state = expenseReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expenses if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-1',
+  };
+  const state = expenseReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test('should add an expense', () => {
+  const newExpense = [{
+    id: '4',
+    description: 'Water Bill',
+    note: '',
+    amount: 2500,
+    createdAt: 0,
+  }];
+  const state = expenseReducer(expenses, { type: 'ADD_EXPENSE', expense: newExpense });
+  expect(state).toEqual([...expenses, newExpense]);
+});
+
+test('should edit an expense', () => {
+  const note = 'some note';
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[1].id,
+    updates: { note },
+  };
+  const state = expenseReducer(expenses, action);
+  expect(state[1].note).toEqual(note);
+});
+
+test('should not remove expenses if id not found', () => {
+  const note = 'some note';
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '-1',
+    updates: { note },
+  };
+  const state = expenseReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
