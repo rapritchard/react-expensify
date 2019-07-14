5b5aea5f16a05c321b6c26d8f31cc8faddee8219
Reece Pritchard <rapritchard96@gmail.com>
2019-07-14 22:05:50 +0100

    Set up project to deploy on heroku

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const path = require('path');
 
 const app = express();
 const publicPath = path.resolve(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!');
 });
