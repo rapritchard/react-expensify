1d017ff309ff6d86eeee70207f326af464f237a1
Reece Pritchard <rapritchard96@gmail.com>
2019-08-18 11:58:01 +0100

    Added private only routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,25 +3,24 @@ import {
   Router, Route, Switch,
 } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
 import Dashboard from '../components/ExpenseDashboard';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import Help from '../components/Help';
 import NotFound from '../components/NotFound';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={Help} />
         <Route component={NotFound} />
       </Switch>
